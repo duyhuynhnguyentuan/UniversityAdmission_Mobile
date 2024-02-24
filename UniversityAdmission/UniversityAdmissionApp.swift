5a7a4ba0455d811efdbf91926b2c08b4eb2f1905
duyhuynhnguyentuan <andyhntd2003@gmail.com>
2024-02-24 08:19:27 +0700

    Initial Commit

---
+++ UniversityAdmission/UniversityAdmissionApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniversityAdmissionApp.swift
+//  UniversityAdmission
+//
+//  Created by Duy Huỳnh Nguyễn Tuấn on 24/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct UniversityAdmissionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
